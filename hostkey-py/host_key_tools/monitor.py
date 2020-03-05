6b13cc6d9cc6995f6657d26f48a945791691680f
Komal Thareja <kthar10@renci.org>
2020-03-05 11:54:14 -0500

    monitoring data pushed to kafka with ssl

--- hostkey-py/host_key_tools/monitor.py
+++ hostkey-py/host_key_tools/monitor.py
@@ -5,6 +5,69 @@ import json
 from kafka import *
 from ssl import create_default_context, Purpose
 
+def _create_ssl_context(*, cafile=None, capath=None, cadata=None,
+                       certfile=None, keyfile=None, password=None,
+                       crlfile=None):
+    """
+    Simple helper, that creates an SSLContext based on params similar to
+    those in ``kafka-python``, but with some restrictions like:
+
+            * ``check_hostname`` is not optional, and will be set to True
+            * ``crlfile`` option is missing. It is fairly hard to test it.
+
+    .. _load_verify_locations: https://docs.python.org/3/library/ssl.html\
+        #ssl.SSLContext.load_verify_locations
+    .. _load_cert_chain: https://docs.python.org/3/library/ssl.html\
+        #ssl.SSLContext.load_cert_chain
+
+    Arguments:
+        cafile (str): Certificate Authority file path containing certificates
+            used to sign broker certificates. If CA not specified (by either
+            cafile, capath, cadata) default system CA will be used if found by
+            OpenSSL. For more information see `load_verify_locations`_.
+            Default: None
+        capath (str): Same as `cafile`, but points to a directory containing
+            several CA certificates. For more information see
+            `load_verify_locations`_. Default: None
+        cadata (str/bytes): Same as `cafile`, but instead contains already
+            read data in either ASCII or bytes format. Can be used to specify
+            DER-encoded certificates, rather than PEM ones. For more
+            information see `load_verify_locations`_. Default: None
+        certfile (str): optional filename of file in PEM format containing
+            the client certificate, as well as any CA certificates needed to
+            establish the certificate's authenticity. For more information see
+            `load_cert_chain`_. Default: None.
+        keyfile (str): optional filename containing the client private key.
+            For more information see `load_cert_chain`_. Default: None.
+        password (str): optional password to be used when loading the
+            certificate chain. For more information see `load_cert_chain`_.
+            Default: None.
+
+    """
+    if cafile or capath:
+        print('Loading SSL CA from %s', cafile or capath)
+    elif cadata is not None:
+        print('Loading SSL CA from data provided in `cadata`')
+        print('`cadata`: %r', cadata)
+    # Creating context with default params for client sockets.
+    context = create_default_context(
+        Purpose.SERVER_AUTH, cafile=cafile, capath=capath, cadata=cadata)
+    # Load certificate if one is specified.
+    if certfile is not None:
+        print('Loading SSL Cert from %s', certfile)
+        if keyfile:
+            if password is not None:
+                print('Loading SSL Key from %s with password', keyfile)
+            else:  # pragma: no cover
+                print('Loading SSL Key from %s without password', keyfile)
+        # NOTE: From docs:
+        # If the password argument is not specified and a password is required,
+        # OpenSSLs built-in password prompting mechanism will be used to
+        # interactively prompt the user for a password.
+        context.load_cert_chain(certfile, keyfile, password)
+    return context
+
+
 class ResourceMonitor():
 
     def __init__(self, topic, kafkaHost='localhost:9092', log=None):
@@ -77,7 +140,7 @@ class ResourceMonitor():
         a.delete_topics(topics, timeout_ms=30)
 
     def _create_ssl(self):
-        context = self.__create_ssl_context(
+        context = _create_ssl_context(
             cafile=self._ssl_cafile,  # CA used to sign certificate.
             # `CARoot` of JKS store container
             certfile=self._ssl_certfile,  # Signed certificate
@@ -86,65 +149,4 @@ class ResourceMonitor():
 
         context.check_hostname = False
         context.verify_mode = False
-        return context
-    def __create_ssl_context(self, *, cafile=None, capath=None, cadata=None,
-                           certfile=None, keyfile=None, password=None,
-                           crlfile=None):
-        """
-        Simple helper, that creates an SSLContext based on params similar to
-        those in ``kafka-python``, but with some restrictions like:
-
-                * ``check_hostname`` is not optional, and will be set to True
-                * ``crlfile`` option is missing. It is fairly hard to test it.
-
-        .. _load_verify_locations: https://docs.python.org/3/library/ssl.html\
-            #ssl.SSLContext.load_verify_locations
-        .. _load_cert_chain: https://docs.python.org/3/library/ssl.html\
-            #ssl.SSLContext.load_cert_chain
-
-        Arguments:
-            cafile (str): Certificate Authority file path containing certificates
-                used to sign broker certificates. If CA not specified (by either
-                cafile, capath, cadata) default system CA will be used if found by
-                OpenSSL. For more information see `load_verify_locations`_.
-                Default: None
-            capath (str): Same as `cafile`, but points to a directory containing
-                several CA certificates. For more information see
-                `load_verify_locations`_. Default: None
-            cadata (str/bytes): Same as `cafile`, but instead contains already
-                read data in either ASCII or bytes format. Can be used to specify
-                DER-encoded certificates, rather than PEM ones. For more
-                information see `load_verify_locations`_. Default: None
-            certfile (str): optional filename of file in PEM format containing
-                the client certificate, as well as any CA certificates needed to
-                establish the certificate's authenticity. For more information see
-                `load_cert_chain`_. Default: None.
-            keyfile (str): optional filename containing the client private key.
-                For more information see `load_cert_chain`_. Default: None.
-            password (str): optional password to be used when loading the
-                certificate chain. For more information see `load_cert_chain`_.
-                Default: None.
-
-        """
-        if cafile or capath:
-            self.logMessage('Loading SSL CA from %s', cafile or capath)
-        elif cadata is not None:
-            self.logMessage('Loading SSL CA from data provided in `cadata`')
-            self.logMessage('`cadata`: %r', cadata)
-        # Creating context with default params for client sockets.
-        context = create_default_context(
-            Purpose.SERVER_AUTH, cafile=cafile, capath=capath, cadata=cadata)
-        # Load certificate if one is specified.
-        if certfile is not None:
-            self.logMessage('Loading SSL Cert from %s', certfile)
-            if keyfile:
-                if password is not None:
-                    self.logMessage('Loading SSL Key from %s with password', keyfile)
-                else:  # pragma: no cover
-                    self.logMessage('Loading SSL Key from %s without password', keyfile)
-            # NOTE: From docs:
-            # If the password argument is not specified and a password is required,
-            # OpenSSL’s built-in password prompting mechanism will be used to
-            # interactively prompt the user for a password.
-            context.load_cert_chain(certfile, keyfile, password)
         return context
